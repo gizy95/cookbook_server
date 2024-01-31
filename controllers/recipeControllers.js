95e5dbb3207ef97eb708dceb4f1ab5acf98c9ad1
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-01-31 18:01:52 +0100

    added logic for fetching one recipe

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -11,12 +11,34 @@ export const getRecipes = async (req, res) => {
     }
 };
 
-export const getRecipe = (req, res) => {
+export const getRecipe = async (req, res) => {
     let { id } = req.params;
-    const recipe = recipes.recipes.find((recipe) => recipe.id === id);
-    if (recipe) {
-        res.json(recipe);
-    } else {
-        res.status(404).send("There is no such recipe");
+    try {
+        const query = `SELECT
+    Recipes.id,
+    Recipes.title,
+    Recipes.servings,
+    Recipes.calories,
+    Recipes.picture,
+    Recipes.difficulty,
+    Recipes.time,
+    Recipes.course,
+    Recipes.steps,
+    STRING_AGG(Ingredients.ingredient_text, ', ') AS all_ingredients
+FROM
+    Recipes
+LEFT JOIN
+    RecipeIngredients ON Recipes.id = RecipeIngredients.recipe_id
+LEFT JOIN
+    Ingredients ON RecipeIngredients.ingredient_id = Ingredients.id
+    WHERE Recipes.id=$1
+GROUP BY
+    Recipes.id, Recipes.title, Recipes.servings, Recipes.calories, Recipes.difficulty, Recipes.time, Recipes.course, Recipes.steps;`
+        const { rows } = await pool.query(query, [id])
+        res.json(rows[0])
+
+    } catch (error) {
+        console.error("Error fetching recipes:", error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 };
